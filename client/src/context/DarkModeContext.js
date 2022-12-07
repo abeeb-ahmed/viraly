36de0eacc320c2659ed9abff84750db9e55df6da
Abeeb Ahmed <abeebahmed@gmail.com>
2022-12-07 13:48:03 +0100

    added darkmode context

---
+++ client/src/context/DarkModeContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export const DarkModeContextProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
+
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+  }, [darkMode]);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
