c2b0d6cf6e05a3f8110caedbdfb26f92fe749503
Abeeb Ahmed <abeebahmed@gmail.com>
2022-12-12 13:29:00 +0100

    added home and profile page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
+import Stories from "../../components/stories/Stories";
+import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
 import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Stories />
+      <Share />
+      <Posts />
+    </div>
+  );
 };
 
 export default Home;
