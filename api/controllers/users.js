afc5a4a56c8cab609664f6fe6c9b0a099607111d
Abeeb Ahmed <abeebahmed@gmail.com>
2022-12-30 16:24:27 +0100

    added update functionality

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,4 +1,5 @@
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 
 export const getUser = (req, res) => {
   let q = "SELECT  * FROM users WHERE id = ? ";
@@ -6,5 +7,36 @@ export const getUser = (req, res) => {
     if (error) return res.status(500).json(error);
 
     return res.status(200).json(data[0]);
+  });
+};
+
+export const updateUser = (req, res) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json("Not logged in");
+
+  jwt.verify(token, process.env.JWT_PASSWORD, (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    const q =
+      "UPDATE users SET `username` = ? `email`= ?, `name` = ?, `city` = ?, `website` = ?, `profilePic` = ?, `coverPic` = ? WHERE `id` = ?";
+
+    db.query(
+      q,
+      [
+        req.body.username,
+        req.body.email,
+        req.body.name,
+        req.body.city,
+        req.body.website,
+        req.body.profilePic,
+        req.body.coverPic,
+        userInfo.id,
+      ],
+      (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("User updated");
+      }
+    );
   });
 };
--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -2,8 +2,10 @@ import "./update.scss";
 import CloseIcon from "@mui/icons-material/Close";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { axiosInstance } from "../../axios";
+import { storage } from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Update = ({ setUpdateOpen }) => {
   const { currentUser } = useContext(AuthContext);
@@ -14,14 +16,132 @@ const Update = ({ setUpdateOpen }) => {
     })
   );
 
-  const { name, setName } = useState(userQuery.data.name);
-  const { email, setEmail } = useState(userQuery.data.email);
-  const { city, setCity } = useState(userQuery.data.city);
-  const { website, setWebsite } = useState(userQuery.data.website);
-  const { coverPicFile, setCoverPicFile } = useState(null);
-  const { profilePicFile, setProfilePicFile } = useState(null);
-  const { profilePic, setProfilePic } = useState(userQuery.data.profilePic);
-  const { coverPic, setCoverPic } = useState(userQuery.data.coverPic);
+  const [name, setName] = useState(userQuery.data.name);
+  const [username, setUsername] = useState(userQuery.data.username);
+  const [email, setEmail] = useState(userQuery.data.email);
+  const [city, setCity] = useState(userQuery.data.city);
+  const [website, setWebsite] = useState(userQuery.data.website);
+  const [coverPicFile, setCoverPicFile] = useState(null);
+  const [profilePicFile, setProfilePicFile] = useState(null);
+  const [profilePic, setProfilePic] = useState(userQuery.data.profilePic);
+  const [coverPic, setCoverPic] = useState(userQuery.data.coverPic);
+
+  const queryClient = useQueryClient();
+
+  // handle update request to mysql
+  const mutation = useMutation(
+    (updatedUser) => {
+      return axiosInstance.put("/users", updatedUser);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["posts"]);
+      },
+    }
+  );
+
+  // handle user profile update
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    // upload profile picture
+    if (profilePicFile) {
+      // Upload file and metadata
+      const storageRef = ref(
+        storage,
+        "images/" + currentUser.name + Date.now()
+      );
+      const uploadTask = uploadBytesResumable(storageRef, profilePicFile);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              return;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setProfilePic(downloadURL);
+          });
+        }
+      );
+    }
+
+    // upload cover picture
+    if (coverPicFile) {
+      // Upload file and metadata
+      const storageRef = ref(
+        storage,
+        "images/" + currentUser.name + Date.now()
+      );
+      const uploadTask = uploadBytesResumable(storageRef, coverPicFile);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              return;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setCoverPic(downloadURL);
+          });
+        }
+      );
+    }
+
+    mutation.mutate({
+      username,
+      email,
+      name,
+      website,
+      city,
+      profilePic,
+      coverPic,
+    });
+    setName("");
+    setUsername("");
+    setEmail("");
+    setWebsite("");
+    setCity("");
+    setProfilePic("");
+    setCoverPic("");
+    setCoverPicFile(null);
+    setProfilePic(null);
+    setUpdateOpen(false);
+  };
 
   return (
     <div className="update">
@@ -37,6 +157,15 @@ const Update = ({ setUpdateOpen }) => {
               placeholder={`${userQuery.data.name || ""}`}
               value={name}
               onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="row">
+            <label>Username</label>
+            <input
+              type="text"
+              placeholder={`${userQuery.data.name || ""}`}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="row">
@@ -73,6 +202,7 @@ const Update = ({ setUpdateOpen }) => {
               name="profilePic"
               id="profilePic"
               accept="image/*"
+              onchange={(e) => setProfilePicFile(e.target.files[0])}
             />
           </div>
           <div className="row">
@@ -82,9 +212,10 @@ const Update = ({ setUpdateOpen }) => {
               name="profilePic"
               id="coverPic"
               accept="image/*"
+              onchange={(e) => setCoverPicFile(e.target.files[0])}
             />
           </div>
-          <button>Update</button>
+          <button onClick={handleUpdate}>Update</button>
         </form>
       </div>
     </div>
